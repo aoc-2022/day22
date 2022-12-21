47425384e5cac0a3c74d3796686240528de5e9d4
xeno <xeno@lv427.space>
2022-12-21 23:09:15 +0100

    created project before day 22

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿open System.IO
+open System
+
+type Foo(s:string) =
+    member this.S = s
+    override this.ToString() = $"Foo({s})"
+
+let parse (s:string) : Foo =
+    match s.Split [|'ø'|] |> Array.toList with
+    | s :: rest -> Foo(s)
+
+let input = File.ReadAllLines "/tmp/aoc/input" |> Seq.map parse |> Seq.toList
+            
+input |> List.map (printfn "%A")    
+    
