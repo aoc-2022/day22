a4b71ca38bf318383adb5360c1410a517053e92c
xeno <xeno@lv427.space>
2022-12-22 19:02:49 +0100

    lol

--- Program.fs
+++ Program.fs
@@ -1,6 +1,8 @@
 ﻿open System.IO
 open System
 
+let inputFile = "/tmp/aoc/output"
+
 type Block =
     | OPEN
     | WALL
@@ -30,7 +32,7 @@ type Inst =
     | Right
     | Steps of int
 
-type CubeInfo(equatorMinY, equatorMaxY, prePoleMinX, prePoleMaxX, poleMinX, poleMaxX, maxY:int, maxX:int) =
+type CubeInfo(equatorMinY, equatorMaxY, prePoleMinX, prePoleMaxX, poleMinX, poleMaxX, maxY: int, maxX: int) =
     member this.EquatorMaxY = equatorMaxY
     member this.EquatorMinY = equatorMinY
     member this.PoleMinX = poleMinX
@@ -62,121 +64,83 @@ type Area(area: Map<Pos, Block>, south: int, east: int) =
 
     member this.Available(pos: Pos) = area.TryFind pos = Some(OPEN)
 
-    member this.NextTile ((x, y): Pos) (dir: Dir) : Pos =
-        let (x, y) = nextPos dir (x, y)
-
-        if area.ContainsKey((x, y)) then
-            (x, y)
-        else
-            match dir with
-            | EAST ->
-                let next = (if x > east then -1 else x), y
-                this.NextTile next dir
-            | WEST ->
-                let next = (if x < 0 then east else x), y
-                this.NextTile next dir
-            | SOUTH ->
-                let next = x, (if y > south then -1 else y)
-                this.NextTile next dir
-            | NORTH ->
-                let next = x, (if y < 0 then south else y)
-                this.NextTile next dir
-
     member this.NextCubeTile (cubeInfo: CubeInfo) ((x, y): Pos) (dir: Dir) : Dir * Pos =
         if area.ContainsKey(nextPos dir (x, y)) then
             dir, nextPos dir (x, y)
         else
-            let orig = (x,y)
+            let orig = (x, y)
+
             match dir with
             | EAST when cubeInfo.EquatorMinY > y -> // OK
-                printfn $"Equator: North {(x, y)} {cubeInfo}"
-                let y = cubeInfo.maxY - y 
+                let y = cubeInfo.maxY - y
                 let x = cubeInfo.maxX
                 let dir = WEST
-                printfn $"EAST ({orig} -> {(x,y)},{dir}"
-                dir,(x,y)
-            | EAST when cubeInfo.EquatorMaxY < y -> // OK 
-                printfn $"Equator: South {(x, y)} {cubeInfo}"
+                dir, (x, y)
+            | EAST when cubeInfo.EquatorMaxY < y -> // OK
                 let x = cubeInfo.PoleMaxX
                 let y = cubeInfo.maxY - y
                 let dir = WEST
-                printfn $"EAST ({orig} -> {(x,y)},{dir}"
-                dir,(x,y)
-            | EAST -> // OK 
-                printfn $"Equator: At {(x, y)} cubeInfo={cubeInfo}"
+                dir, (x, y)
+            | EAST -> // OK
                 let x = cubeInfo.EquatorMaxY - y + cubeInfo.PoleMaxX + 1
                 let y = cubeInfo.EquatorMaxY + 1
                 let dir = SOUTH
-                printfn $"EAST ({orig} -> {(x,y)},{dir}"
-                dir,(x,y)
+                dir, (x, y)
             | WEST when y < cubeInfo.EquatorMinY -> // OK
-                printfn $"Equator: South {(x, y)} {cubeInfo}"
                 let x = cubeInfo.PrePoleMinX + y
                 let y = cubeInfo.EquatorMinY
-                let dir = SOUTH 
-                printfn $"WEST ({orig} -> {(x,y)},{dir}"
-                dir,(x,y)
+                let dir = SOUTH
+                dir, (x, y)
             | WEST when y > cubeInfo.EquatorMaxY -> // OK
-                printfn $"Equator: South {(x, y)} {cubeInfo}"
                 let x = cubeInfo.maxY - y + cubeInfo.PrePoleMinX
                 let y = cubeInfo.EquatorMaxY
-                let dir = NORTH 
-                printfn $"WEST ({orig} -> {(x,y)},{dir}"
-                dir,(x,y)
-            | WEST -> // OK (?) /// TODO VERIFY 
+                let dir = NORTH
+                dir, (x, y)
+            | WEST -> // OK (?) /// TODO VERIFY
                 let x = cubeInfo.maxX - (y - cubeInfo.EquatorMinY)
                 let y = cubeInfo.maxY
-                let dir = NORTH 
-                printfn $"WEST ({orig} -> {(x,y)},{dir}"
-                dir,(x,y)
-            | SOUTH when x < cubeInfo.PrePoleMinX -> // OK 
+                let dir = NORTH
+                dir, (x, y)
+            | SOUTH when x < cubeInfo.PrePoleMinX -> // OK
                 let x = cubeInfo.PoleMaxX - x
                 let y = cubeInfo.maxY
-                let dir = NORTH 
-                printfn $"SOUTH ({orig} -> {(x,y)},{dir}"
-                dir,(x,y)
+                let dir = NORTH
+                dir, (x, y)
             | SOUTH when x < cubeInfo.PoleMinX -> // OK
                 let y = cubeInfo.EquatorMaxY + (cubeInfo.PoleMinX - x)
                 let x = cubeInfo.PoleMinX
-                let dir = EAST 
-                printfn $"SOUTH ({orig} -> {(x,y)},{dir}"
-                dir,(x,y)
-            | SOUTH when x > cubeInfo.PoleMaxX -> // OK 
-                let y = cubeInfo.EquatorMinY + cubeInfo.maxX - x 
+                let dir = EAST
+                dir, (x, y)
+            | SOUTH when x > cubeInfo.PoleMaxX -> // OK
+                let y = cubeInfo.EquatorMinY + cubeInfo.maxX - x
                 let x = 0
                 let dir = EAST
-                printfn $"SOUTH ({orig} -> {(x,y)},{dir}"
-                dir,(x,y)
+                dir, (x, y)
             | SOUTH -> // OK
                 let x = cubeInfo.PoleMaxX - x
                 let y = cubeInfo.EquatorMaxY
-                let dir = NORTH 
-                printfn $"SOUTH ({orig} -> {(x,y)},{dir}"
-                dir,(x,y)
+                let dir = NORTH
+                dir, (x, y)
             | NORTH when x < cubeInfo.PrePoleMinX -> // OK
                 let x = cubeInfo.PoleMaxX - x
                 let y = 0
-                let dir = SOUTH 
-                printfn $"NORTH ({orig} -> {(x,y)},{dir}"
-                dir,(x,y)
+                let dir = SOUTH
+                dir, (x, y)
             | NORTH when x < cubeInfo.PoleMinX -> // OK
                 let y = x - cubeInfo.PrePoleMinX
                 let x = cubeInfo.PoleMinX
                 let dir = EAST
-                printfn $"NORTH ({orig} -> {(x,y)},{dir}"
-                dir,(x,y)
+                dir, (x, y)
             | NORTH when x > cubeInfo.PoleMaxX ->
                 let y = cubeInfo.maxX - x + cubeInfo.EquatorMinY
                 let x = cubeInfo.PoleMaxX
                 let dir = WEST
-                printfn $"NORTH ({orig} -> {(x,y)},{dir}"
-                dir,(x,y)
+                dir, (x, y)
             | NORTH ->
-                let x = cubeInfo.PoleMaxX - x 
+                let x = cubeInfo.PoleMaxX - x
                 let y = cubeInfo.EquatorMinY
-                let dir = SOUTH 
-                printfn $"NORTH ({orig} -> {(x,y)},{dir}"
-                dir,(x,y)
+                let dir = SOUTH
+                dir, (x, y)
 
 
 
@@ -184,11 +148,8 @@ type Area(area: Map<Pos, Block>, south: int, east: int) =
 
 
 let initCubeInfo (area: Area) =
-    let wrapX =
-        area.Area.Keys |> Seq.filter (fun (x, y) -> y = 0) |> Seq.map fst |> Seq.max
-
     let maxY = area.Area.Keys |> Seq.map snd |> Seq.max
-    let maxX = area.Area.Keys |> Seq.map fst |> Seq.max 
+    let maxX = area.Area.Keys |> Seq.map fst |> Seq.max
 
     let equatorMinY =
         area.Area.Keys |> Seq.filter (fun (x, y) -> x = 0) |> Seq.map snd |> Seq.min
@@ -198,7 +159,7 @@ let initCubeInfo (area: Area) =
 
     let poleMinX =
         area.Area.Keys |> Seq.filter (fun (x, y) -> y = maxY) |> Seq.map fst |> Seq.min
-    // let poleMaxX = area.Area.Keys |> Seq.filter (fun (x,y) -> y = maxY) |> Seq.map fst |> Seq.max
+
     let poleMaxX =
         area.Area.Keys |> Seq.filter (fun (x, y) -> y = 0) |> Seq.map fst |> Seq.max
 
@@ -239,58 +200,33 @@ let parse (s: string list) : Area * Instructions =
     let east = 1 + (s[0 .. s.Length - 2] |> List.map (String.length) |> List.max)
     Area(area, south, east), instructions
 
-let area, instructions =
-    File.ReadAllLines "/tmp/aoc/input" |> Array.toList |> parse
+let area, instructions = File.ReadAllLines inputFile |> Array.toList |> parse
 
 area |> (printfn "%A")
 instructions |> printfn "%A"
 
 printfn $"start {area.StartTile}"
 
-printfn
-
 type State(cubeInfo: CubeInfo, area: Area, pos: Pos, dir: Dir) =
     member this.Area = area
     member this.Pos = pos
     member this.Dir = dir
 
-    member this.ApplyInstruction(inst: Inst) =
-        printfn $"ApplyInstruction  {inst} {pos} {dir}"
-        match inst with
-        | Left ->
-            printfn $"turn to: {turnLeft dir}"
-            State(cubeInfo, area, pos, turnLeft dir)
-        | Right ->
-            printfn $"turn to: {turnRight dir}"
-            State(cubeInfo, area, pos, turnRight dir)
-        | Steps n ->
-            if n = 0 then
-                this
-            else
-                let dir, nextPos = area.NextCubeTile cubeInfo pos dir
-
-                if area.Available nextPos then
-                    printfn $"Move to: {nextPos} {dir}"
-                    State(cubeInfo, area, nextPos, dir).ApplyInstruction(Steps(n - 1))
-                else
-                    this
-
     member this.ApplyCubeInstruction(inst: Inst) =
         printfn $"ApplyCubeInstruction {inst} {pos} {dir}"
+
         match inst with
-        | Left ->
-            State(cubeInfo, area, pos, turnLeft dir)
-        | Right ->
-            State(cubeInfo, area, pos, turnRight dir)
+        | Left -> State(cubeInfo, area, pos, turnLeft dir)
+        | Right -> State(cubeInfo, area, pos, turnRight dir)
         | Steps n ->
             if n = 0 then
                 this
             else
-                let dir, nextPos = area.NextCubeTile cubeInfo pos dir
-                printfn $"  got {pos} -> {nextPos}"
+                let nextDir, nextPos = area.NextCubeTile cubeInfo pos dir
+                printfn $"  got {pos} {dir} -> {nextPos} {nextDir}"
 
                 if area.Available nextPos then
-                    State(cubeInfo, area, nextPos, dir).ApplyCubeInstruction(Steps(n - 1))
+                    State(cubeInfo, area, nextPos, nextDir).ApplyCubeInstruction(Steps(n - 1))
                 else
                     this
 
@@ -299,20 +235,6 @@ type State(cubeInfo: CubeInfo, area: Area, pos: Pos, dir: Dir) =
 let initState (area: Area) =
     let cubeInfo = initCubeInfo area
     State(cubeInfo, area, area.StartTile, EAST)
-
-let solve1 (area: Area) (instructions: Instructions) =
-    let state = initState area
-
-    let rec solve1 (state: State) (instructions: Instructions) =
-        if instructions.Inst.IsEmpty then
-            state
-        else
-            printfn $"solve1 moving: {state} {instructions.Inst}"
-            let state = state.ApplyInstruction(instructions.Inst.Head)
-            printfn $"  -> {state}"
-            solve1 state (Instructions(instructions.Inst.Tail))
-
-    solve1 state instructions
 
 let solve2 (area: Area) (instructions: Instructions) =
     let state = initState area
@@ -327,11 +249,6 @@ let solve2 (area: Area) (instructions: Instructions) =
             solve state (Instructions(instructions.Inst.Tail))
 
     solve state instructions
-
-
-// let state = solve1 area instructions
-
-// printfn $"state = {state}"
 
 let runAll () =
     let state = solve2 area instructions
@@ -350,7 +267,7 @@ let runAll () =
             | WEST -> 2
             | NORTH -> 3
 
-        (y * 1000) + (4 * x) + dirScore
+        (y * 1000) + (4 * (x - 50)) + dirScore
 
     let task1 = score state
     printfn $"RES 1 {task1}"
@@ -359,6 +276,79 @@ let cubeInfo = initCubeInfo area
 
 cubeInfo |> printfn "%A"
 
-area.NextCubeTile cubeInfo (15,10) EAST |> printfn "%A"
+area.NextCubeTile cubeInfo (100, 149) WEST |> printfn "%A"
 
-// runAll () 
+// runAll ()
+
+let all =
+    area.Area.Keys
+    |> Seq.map (fun pos -> [ (pos, NORTH); (pos, SOUTH); (pos, WEST); (pos, EAST) ])
+    |> Seq.toList
+    |> List.concat
+
+let opposite (dir: Dir) =
+    match dir with
+    | EAST -> WEST
+    | WEST -> EAST
+    | NORTH -> SOUTH
+    | SOUTH -> NORTH
+
+
+let checkReflective (area: Area) ((pos, dir): Pos * Dir) =
+    let newDir, newPos = area.NextCubeTile cubeInfo pos dir
+    let oldDir, oldPos = area.NextCubeTile cubeInfo newPos (opposite newDir)
+    let dirChanged = opposite (oldDir) <> dir
+    let posChanged = oldPos <> pos
+
+    if dirChanged || posChanged then
+        Some(pos, dir, newPos, newDir, oldPos, opposite oldDir)
+    else
+        None
+
+
+
+// checkReflective area (all.Head)
+
+let checkReflectiveAll () =
+    let errors =
+        all |> List.map (fun pd -> checkReflective area pd) |> List.filter Option.isSome
+
+    errors |> List.map (printfn "%A")
+
+printfn $"{cubeInfo.maxY}"
+
+// checkReflectiveAll ()
+
+// runAll()
+
+// 64 94 NORTH
+
+printfn $"{cubeInfo}"
+
+let calcHackyPos (x: int, y: int, dir: Dir) : Pos * Dir =
+    let side =
+        match (x, y) with
+        | _ when y < 50 -> 'a'
+        | _ when y > 99 && x > 149 -> 'b'
+        | _ when y < 100 && x > 99 -> 'c'
+        | _ when y > 99 && x < 140 -> 'd'
+        | _ when y < 100 && x > 49 && x < 100 -> 'e'
+        | _ when y < 100 && x < 50 -> 'f'
+
+    printfn $"side={side}"
+    let x = x % 50
+    let y = y % 50
+
+    let (x, y) =
+        match side with
+        | 'a' -> (x + 50, y)
+        | 'b' -> (150 - x, 50 - y)
+        | 'c' -> (x + 50, y + 50)
+        | 'd' -> (y + 100, 50 - x)
+        | 'e' -> (x + 50, y + 100)
+        | 'f' -> (y, 50 - x + 150)
+
+    (x + 1, y + 1), dir
+
+let hackyPos = calcHackyPos (64, 94, NORTH)
+printfn $"hack pos = {hackyPos}"
